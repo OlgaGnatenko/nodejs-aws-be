4046b7ba7edd1618da7bb66746c45ee1fb9afa09
Olga Kostrub <lady.irony.ua@gmail.com>
2020-11-03 20:36:17 +0200

    feat: aws task 3

---
+++ product-service/productById.js
@@ -0,0 +1,36 @@
+"use strict";
+const products = require("./products");
+
+module.exports.getProductById = async (event) => {
+  const { id } = event.queryStringParameters;
+  if (!id)
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      statusCode: 404,
+      body: "Id is missing in the query parameters",
+    };
+  const product = products.find((item) => item.id === id);
+  if (!product)
+    return {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      statusCode: 404,
+      body: `Product with ${id} is not found`,
+    };
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(product),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
---
+++ product-service/productList.js
@@ -0,0 +1,16 @@
+"use strict";
+const products = require("./products");
+
+module.exports.getProducts = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(products),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
