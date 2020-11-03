ddd931d3e7fbe6c6b4b5adb134c0cceca82a062e
Olga Kostrub <lady.irony.ua@gmail.com>
2020-11-03 21:05:47 +0200

    fix: add try catch

--- product-service/productList.js
+++ product-service/productList.js
@@ -2,14 +2,25 @@
 const products = require("./products");
 
 module.exports.getProducts = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(products),
-  };
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(products),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: `Server error: ${JSON.stringify(err.message)}`,
+    };
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
