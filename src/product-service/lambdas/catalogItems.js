4871190c6b62e964906949d33cc92d49f5accabf
Olga Kostrub <lady.irony.ua@gmail.com>
2020-11-24 21:58:31 +0200

    feat: AWS task 6.3

--- src/product-service/lambdas/catalogItems.js
+++ src/product-service/lambdas/catalogItems.js
@@ -1,3 +1,4 @@
+const AWS = require("aws-sdk");
 const { postBatchProducts } = require("./postBatchProducts");
 
 const catalogBatchProcess = async (event) => {
@@ -6,6 +7,14 @@ const catalogBatchProcess = async (event) => {
     console.log("Products to be added:", products);
     const data = await postBatchProducts(products);
     console.log(`Successfully added products: ${data}`);
+    const sns = new AWS.SNS({
+      region: 'eu-west-1'
+    });
+    await sns.publish({
+      Subject: `${data.length} products were added to database`,
+      Message: JSON.stringify(data),
+      TopicArn: process.env.SQS_ARN
+    }).promise();
     return {
       status: true,
       data,
